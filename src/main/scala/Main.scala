4ec20a11a07a6de34ab67da44fe960de029742b7
Julien de Castelnau <julien.decastelnau@epfl.ch>
2023-12-01 23:15:13 +0100

    main skeleton

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,37 @@
+import scala.collection.mutable
 object Project {
+    trait Compilable {
+        def visit(sb: StringBuilder): Unit
+    }
+
+    case class FunctionSpec (
+    )
+
+    case class SystolicSpec (
+        name: String,
+        inputSpec: FunctionSpec,
+        behavioralSpec: FunctionSpec,
+        structuralSpec: FunctionSpec,
+        outputSpec: FunctionSpec
+    ) extends Compilable  {
+        def visit(sb: StringBuilder): Unit = {
+            sb ++= s"object $name {\n"
+            // Fill in the rest of the fucking compiler
+            sb ++= s"}"
+        }
+    }
+
+    
     def main(args: Array[String]): Unit = {
-        println("Hello!")
+        val s = SystolicSpec(
+            "Gemv",
+            FunctionSpec(),
+            FunctionSpec(),
+            FunctionSpec(),
+            FunctionSpec(),
+        )
+        val sb = mutable.StringBuilder()
+        s.visit(sb)
+        println(sb.toString())
     }
 }
